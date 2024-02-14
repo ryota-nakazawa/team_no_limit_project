908d4aa90ee1f2aa72b765dc26544c6e662eaea2
Akari <65846922+Akari-Oishi@users.noreply.github.com>
2024-02-14 14:56:55 +0900

    Update Speak.js

--- src/components/Speak.js
+++ src/components/Speak.js
@@ -1,20 +1,28 @@
 // Chat GPTの応答を音声で読み上げる関数
-
-export const speak = (text, isSpeaking, language, videoRef, setIsSpeaking) => {
-  const utterance = new SpeechSynthesisUtterance(text);
-  utterance.lang = language;
-  speechSynthesis.speak(utterance);
-  setIsSpeaking(true);
-  if (videoRef.current) {
-    videoRef.current.play(); // 発話が始まったらビデオを再生
-  }
-
-  utterance.onend = () => {
-    setIsSpeaking(false); // 読み上げが終了したことを示す
+export const speak = (
+  text,
+  isSpeaking,
+  isVoiceEnabled,
+  language,
+  videoRef,
+  setIsSpeaking
+) => {
+  if (isVoiceEnabled) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = language;
+    speechSynthesis.speak(utterance);
+    setIsSpeaking(true);
     if (videoRef.current) {
-      videoRef.current.pause(); // ビデオを一時停止
+      videoRef.current.play(); // 発話が始まったらビデオを再生
     }
-  };
+
+    utterance.onend = () => {
+      setIsSpeaking(false); // 読み上げが終了したことを示す
+      if (videoRef.current) {
+        videoRef.current.pause(); // ビデオを一時停止
+      }
+    };
+  }
 };
 
 export default speak;
